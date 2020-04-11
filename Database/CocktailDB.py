136dea11092408b15dcffddbdf315f5bd76fbf60
Kelvin Basdeo <34067705+kbasdeo000@users.noreply.github.com>
2020-04-11 15:09:31 -0400

    Update CocktailDB.py

--- Database/CocktailDB.py
+++ Database/CocktailDB.py
@@ -1,17 +1,43 @@
+#12.42.205.8
 import mysql.connector
-from mysql.connector import errorcode
+from flask import Flask, render_template, request, redirect
 
-try:
-  cnx = mysql.connector.connect(user = 'ml1774', password = 'ml1774123', host = 'localhost', database = 'ml1774')
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
-else:
-    print("Connection Established")
-    cnx.close()
+app = Flask(__name__)
 
-#pip install --user mysql-connector-python
+#conn = mysql.connector.connect(user='root', password='Matter1332*', host='127.0.0.1', database='cocktail')
+conn = mysql.connector.connect(user='GQ7oHUjvYc', password='hpLN89qQdb', host='remotemysql.com', database='GQ7oHUjvYc')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method =='POST':
+        #fetch form data
+        userDetails = request.form
+        cockName = userDetails['cocktail']
+        cockIngredient = userDetails['ingredient']
+        # Creating a cursor object using the cursor() method
+        cursor = conn.cursor()
+
+        # Preparing SQL query to INSERT a record into the database.
+        insert_stmt = (
+            "INSERT INTO cocktails(cocktailName,ingredients)"
+            "VALUES(%s, %s)"
+        )
+        data_input = (cockName, cockIngredient)
+        cursor.execute(insert_stmt, data_input)
+        conn.commit()
+        # Closing the connection
+        cursor.close()
+        conn.close()
+        return redirect('/cocktails')
+    return render_template('index.html')
+
+
+@app.route('/cocktails')
+def cocktails():
+    # Creating a cursor object using the cursor() method
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM cocktails")
+    userDetails = cursor.fetchall()
+    return render_template('cocktails.html', userDetails=userDetails)
+
+if __name__ == '__main__':
+    app.run(debug=True)
