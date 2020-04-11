e04fb0699da28fb61e91ef3a6b9c2458cc02b72a
matter13311 <33920713+matter13311@users.noreply.github.com>
2020-04-11 15:47:29 -0400

    added new code that uses flask-mysqldb instead of mysqlconnector

--- Database/CocktailDB.py
+++ Database/CocktailDB.py
@@ -1,11 +1,18 @@
 #12.42.205.8
 import mysql.connector
 from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] ='remotemysql.com'
+app.config['MYSQL_USER'] ='GQ7oHUjvYc'
+app.config['MYSQL_PASSWORD'] ='hpLN89qQdb'
+app.config['MYSQL_DB'] ='GQ7oHUjvYc'
+
+mysql = MySQL(app)
 
 #conn = mysql.connector.connect(user='root', password='Matter1332*', host='127.0.0.1', database='cocktail')
-conn = mysql.connector.connect(user='GQ7oHUjvYc', password='hpLN89qQdb', host='remotemysql.com', database='GQ7oHUjvYc')
+#conn = mysql.connector.connect(user='GQ7oHUjvYc', password='hpLN89qQdb', host='remotemysql.com', database='GQ7oHUjvYc')
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method =='POST':
@@ -14,7 +21,7 @@ def index():
         cockName = userDetails['cocktail']
         cockIngredient = userDetails['ingredient']
         # Creating a cursor object using the cursor() method
-        cursor = conn.cursor()
+        cursor = mysql.connection.cursor()
 
         # Preparing SQL query to INSERT a record into the database.
         insert_stmt = (
@@ -23,10 +30,10 @@ def index():
         )
         data_input = (cockName, cockIngredient)
         cursor.execute(insert_stmt, data_input)
-        conn.commit()
+        mysql.connection.commit()
         # Closing the connection
         cursor.close()
-        conn.close()
+        #return 'success'
         return redirect('/cocktails')
     return render_template('index.html')
 
@@ -34,7 +41,7 @@ def index():
 @app.route('/cocktails')
 def cocktails():
     # Creating a cursor object using the cursor() method
-    cursor = conn.cursor()
+    cursor = mysql.connection.cursor()
     cursor.execute("SELECT * FROM cocktails")
     userDetails = cursor.fetchall()
     return render_template('cocktails.html', userDetails=userDetails)
