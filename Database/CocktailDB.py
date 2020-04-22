2b1fc05c37b5b847f74770d24948ca026e8f2fc0
Kelvin Basdeo <kelvin@Kelvins-MBP.home>
2020-04-22 17:14:55 -0400

    Added Queries

--- Database/CocktailDB.py
+++ Database/CocktailDB.py
@@ -1,5 +1,18 @@
 #12.42.205.8
 
+
+# This should be connected to the "Get Recipe" button
+#select distinct r1.RecipeName, r1.Spirit, r1.SpiritAmount,  r1.Syrup, r1.SyrupAmount,  r1.Juice, r1.JuiceAmount,  r1.Fruit, r1.FruitAmount
+#from Ingredients r1
+#join UserIngredients u1
+#on  r1.Spirit like u1.Ingredient
+#or  r1.Syrup like u1.Ingredient
+#or  r1.Juice like u1.Ingredient
+#or  r1.Fruit like u1.Ingredient;
+
+# This should be connected to the "New Recipe" button
+#delete from UserIngredients;
+
 from flask import Flask, render_template, request, redirect, jsonify
 from flaskext.mysql import MySQL
 import pymysql
@@ -23,8 +36,9 @@ def index():
         cursor = mysql.connection.cursor()
 
         # Preparing SQL query to INSERT a record into the database.
+        # This should be connected to the "Done" button.
         insert_stmt = (
-            "INSERT INTO Cocktail(RecipeID,RecipeName)"
+            "INSERT INTO UserIngredients(UserID,Ingredient)"
             "VALUES(%s, %s)"
         )
         data_input = (cockName, cockIngredient)
